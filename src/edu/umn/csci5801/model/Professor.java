bdf8b85559316ff27186665ac99a5f91e9d09436
Justin Wells <wells369@umn.edu>
2014-12-03 00:31:23 -0600

    Professor.java

--- src/edu/umn/csci5801/model/Professor.java
+++ src/edu/umn/csci5801/model/Professor.java
@@ -4,4 +4,22 @@ package edu.umn.csci5801.model;
  * Created by Justin on 12/2/2014.
  */
 public class Professor {
+    private Department department;
+    private String firstName;
+    private String lastName;
+
+    public Professor() {}
+    public Professor(Department d, String fn, String ln) {
+        department = d;
+        firstName = fn;
+        lastName = ln;
+    }
+
+    public void setDepartment(Department d) { department = d; }
+    public void setFirstName(String fn) { firstName = fn; }
+    public void setLastName(String ln) { lastName = ln; }
+
+    public Department getDepartment() { return department; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
 }
