9f44d5e83c6ec705ba879363ebdfe76d092edc93
Justin Wells <wells369@umn.edu>
2014-12-03 12:41:20 -0600

    2 files

--- src/edu/umn/csci5801/GRADS.java
+++ src/edu/umn/csci5801/GRADS.java
@@ -18,8 +18,8 @@ import edu.umn.csci5801.GRADSIntf;
 
 
 /**
-  * Your implementation of this interface must be named GRADS
-  */
+ * Your implementation of this interface must be named GRADS
+ */
 public class GRADS implements GRADSIntf
 {
     private List<User> userList;
@@ -28,191 +28,209 @@ public class GRADS implements GRADSIntf
     private List<Course> courseList;
     private List<StudentRecord> recordList;
 
-        /**
-         * Loads the list of system usernames and permissions.
-         * @param usersFile the filename of the users file.
-         * @throws Exception for I/O errors.  SEE NOTE IN CLASS HEADER.
-         */
-        public void loadUsers(String usersFile) throws Exception{
-            try {
-                List<User> users = new Gson().fromJson( new FileReader( new File(usersFile)), new TypeToken<List<User>>(){}.getType());
-                this.userList = users;
-            } catch (IOException ioException) {
-                System.out.println("IO Exception while Loading Users");
-            }
+    /**
+     * Loads the list of system usernames and permissions.
+     * @param usersFile the filename of the users file.
+     * @throws Exception for I/O errors.  SEE NOTE IN CLASS HEADER.
+     */
+    public void loadUsers(String usersFile) throws Exception{
+        try {
+            List<User> users = new Gson().fromJson( new FileReader( new File(usersFile)), new TypeToken<List<User>>(){}.getType());
+            this.userList = users;
+        } catch (IOException ioException) {
+            System.out.println("IO Exception while Loading Users");
         }
+    }
 
-        /**
-         * Loads the list of courses.
-         * @param coursesFile the filename of the users file.
-         * @throws Exception for I/O errors.  SEE NOTE IN CLASS HEADER.
-         */
-        public void loadCourses(String coursesFile) throws Exception{
-            try {
-                List<Course> courses = new Gson().fromJson( new FileReader( new File(coursesFile)), new TypeToken<List<Course>>(){}.getType());
-                this.courseList = courses;
-            } catch (IOException ioException) {
-                System.out.println("IO Exception while Loading Courses");
-            }
+    /**
+     * Loads the list of courses.
+     * @param coursesFile the filename of the users file.
+     * @throws Exception for I/O errors.  SEE NOTE IN CLASS HEADER.
+     */
+    public void loadCourses(String coursesFile) throws Exception{
+        try {
+            List<Course> courses = new Gson().fromJson( new FileReader( new File(coursesFile)), new TypeToken<List<Course>>(){}.getType());
+            this.courseList = courses;
+        } catch (IOException ioException) {
+            System.out.println("IO Exception while Loading Courses");
         }
+    }
 
-        /**
-         * Loads the list of system transcripts.
-         * @param recordsFile the filename of the transcripts file.
-         * @throws Exception for I/O errors.  SEE NOTE IN CLASS HEADER.
-         */
-        public void loadRecords(String recordsFile) throws Exception {
-            try {
-                List<StudentRecord> records = new Gson().fromJson( new FileReader( new File(recordsFile)), new TypeToken<List<Course>>(){}.getType());
-                this.recordList = records;
-            } catch (IOException ioException) {
-                System.out.println("IO Exception while Loading Records");
-            }
+    /**
+     * Loads the list of system transcripts.
+     * @param recordsFile the filename of the transcripts file.
+     * @throws Exception for I/O errors.  SEE NOTE IN CLASS HEADER.
+     */
+    public void loadRecords(String recordsFile) throws Exception {
+        try {
+            List<StudentRecord> records = new Gson().fromJson( new FileReader( new File(recordsFile)), new TypeToken<List<Course>>(){}.getType());
+            this.recordList = records;
+        } catch (IOException ioException) {
+            System.out.println("IO Exception while Loading Records");
         }
+    }
 
-        /**
-         * Sets the user id (X500) of the user currently using the system.
-         * @param userId  the X500 id of the user generating progress summaries.
-         * @throws Exception  if the user id is invalid.  SEE NOTE IN CLASS HEADER.
-         */
-        public void setUser(String userId) throws Exception {
-            //loadUsers();
-            if(userList == null) {
-                Exception exception = new Exception();
-                throw exception;
-            }
-            else {
-                boolean assigned = false;
-                for (User u : userList) {
-                    if (userId.equals(u.getId())) {
-                        this.currentUser = u;
-                        assigned = true;
-                    }
-                }
-                if (!assigned) {
-                    Exception exception = new Exception();
-                    throw exception;
-                }
-            }
+    /**
+     * Sets the user id (X500) of the user currently using the system.
+     * @param userId  the X500 id of the user generating progress summaries.
+     * @throws Exception  if the user id is invalid.  SEE NOTE IN CLASS HEADER.
+     */
+    public void setUser(String userId) throws Exception {
+        //loadUsers();
+        if(userList == null) {
+            Exception exception = new Exception();
+            throw exception;
         }
-
-        /**
-         * Gets the user id of the user currently using the system.
-         * @return  the X500 user id of the user currently using the system.
-         */
-        public String getUser() {
-            if(this.currentUser == null) {
-                System.out.println("User Not Assigned");
-                return null;
-            }
-            return this.currentUser.getId();
-        }
-
-        /**
-         * Gets a list of the userIds of the students that a GPC can view.
-         * @return a list containing the userId of for each student in the
-         *      system belonging to the current user
-         * @throws Exception is the current user is not a GPC.
-         */
-        public List<String> getStudentIDs() throws Exception {
-            if(this.currentUser.getRole() == Role.STUDENT || userList == null){
-                Exception exception = new Exception();
-                throw exception;
-            }
-            List<String> studentIDList = new ArrayList<String>();
-            try {
-                for (User u : userList) {
-                    studentIDList.add(u.getId());
-                }
-            } catch(Exception e){
-                throw e;
-            }
-            return studentIDList;
-        }
-
-        /**
-         * Gets the raw student record data for a given userId.
-         * @param userId  the identifier of the student.
-         * @return  the student record data.
-         * @throws Exception  if the form data could not be retrieved.  SEE NOTE IN
-         *      CLASS HEADER.
-         */
-        public StudentRecord getTranscript(String userId) throws Exception {
-            StudentRecord studentRecord = new StudentRecord();
-            return studentRecord;
-        }
-
-        /**
-         * Saves a new set of student data to the records data.
-         * @param userId the student ID to overwrite.
-         * @param transcript  the new student record
-         * @throws Exception  if the transcript data could not be saved, failed
-         * a validity check, or a non-GPC tries to call.  SEE NOTE IN CLASS HEADER.
-         */
-        public void updateTranscript(String userId, StudentRecord transcript) throws Exception {
-            return;
-        }
-
-        /**
-         * Appends a note to a student record.
-         * @param userId the student ID to add a note to.
-         * @param note  the note to append
-         * @throws Exception  if the note could not be saved or a non-GPC tries to call.
-         * SEE NOTE IN CLASS HEADER.
-         */
-        public void addNote(String userId, String note) throws Exception {
-            return;
-        }
-
-        /**
-         * Generates progress summary
-         * @param userId the student to generate the record for.
-         * @returns the student's progress summary in a data class matching the JSON format.
-         * @throws Exception  if the progress summary could not be generated.
-         * SEE NOTE IN CLASS HEADER.
-         */
-        public ProgressSummary generateProgressSummary(String userId) throws Exception {
-            boolean realUser = false;
-            User desiredUser = new User();
-            for(User u : userList) {
+        else {
+            boolean assigned = false;
+            for (User u : userList) {
                 if (userId.equals(u.getId())) {
-                    desiredUser = u;
-                    realUser = true;
+                    this.currentUser = u;
+                    assigned = true;
                 }
             }
-            //check if correct permissions for accessing student progress summary
-            if(realUser &&
-                    ((currentUser.getRole().equals(Role.GRADUATE_PROGRAM_COORDINATOR) &&
-                            currentUser.getDepartment().equals(desiredUser.getDepartment())) ||
-                            currentUser.getId().equals(userId))) {
-                //finds studentRecord from data passed in with loadRecords and gets progressSummary
-                for(StudentRecord sr : recordList) {
-                    if (userId.equals(sr.getStudent().getId())) {
-                        ProgressSummary progressSummary = new ProgressSummary(sr.getStudent(),
-                                sr.getDepartment(), sr.getDegreeSought(), sr.getTermBegan(),
-                                sr.getAdvisor(), sr.getCommittee(), sr.getNotes());
-                        progressSummary.checkGradStatus();
-                        return progressSummary;
-                    }
-                }
-            }
-            else{
-                //throw exception
+            if (!assigned) {
+                Exception exception = new Exception();
+                throw exception;
             }
         }
+    }
 
-        /**
-         * Generates a new progress summary, assuming that the student passes the
-         * provided set of prospective courses.
-         * @param userId the student to generate the record for.
-         * @param courses a list of the prospective courses.
-         * @returns a map containing the student's hypothetical progress summary
-         * @throws Exception  if the progress summary could not be generated or the courses
-         * are invalid. SEE NOTE IN CLASS HEADER.
-         */
-        public ProgressSummary simulateCourses(String userId, List<CourseTaken> courses) throws Exception {
-            ProgressSummary progressSummary = new ProgressSummary();
-            return progressSummary;
+    /**
+     * Gets the user id of the user currently using the system.
+     * @return  the X500 user id of the user currently using the system.
+     */
+    public String getUser() {
+        if(this.currentUser == null) {
+            System.out.println("User Not Assigned");
+            return null;
         }
+        return this.currentUser.getId();
+    }
+
+    /**
+     * Gets a list of the userIds of the students that a GPC can view.
+     * @return a list containing the userId of for each student in the
+     *      system belonging to the current user
+     * @throws Exception is the current user is not a GPC.
+     */
+    public List<String> getStudentIDs() throws Exception {
+        if(this.currentUser.getRole() == Role.STUDENT || userList == null){
+            Exception exception = new Exception();
+            throw exception;
+        }
+        List<String> studentIDList = new ArrayList<String>();
+        try {
+            for (User u : userList) {
+                studentIDList.add(u.getId());
+            }
+        } catch(Exception e){
+            throw e;
+        }
+        return studentIDList;
+    }
+
+    /**
+     * Gets the raw student record data for a given userId.
+     * @param userId  the identifier of the student.
+     * @return  the student record data.
+     * @throws Exception  if the form data could not be retrieved.  SEE NOTE IN
+     *      CLASS HEADER.
+     */
+    public StudentRecord getTranscript(String userId) throws Exception {
+        StudentRecord studentRecord = new StudentRecord();
+        return studentRecord;
+    }
+
+    /**
+     * Saves a new set of student data to the records data.
+     * @param userId the student ID to overwrite.
+     * @param transcript  the new student record
+     * @throws Exception  if the transcript data could not be saved, failed
+     * a validity check, or a non-GPC tries to call.  SEE NOTE IN CLASS HEADER.
+     */
+    public void updateTranscript(String userId, StudentRecord transcript) throws Exception {
+        return;
+    }
+
+    /**
+     * Appends a note to a student record.
+     * @param userId the student ID to add a note to.
+     * @param note  the note to append
+     * @throws Exception  if the note could not be saved or a non-GPC tries to call.
+     * SEE NOTE IN CLASS HEADER.
+     */
+    public void addNote(String userId, String note) throws Exception {
+        if(this.recordList == null) {
+            Exception e = new Exception();
+            throw e;
+        }
+        int index = -1, count = 0;
+        for(StudentRecord sRecord : this.recordList) {
+            if(userId.equals(sRecord.getStudent().getId()))
+                index = count;
+            count++;
+        }
+        if(index == -1){
+            Exception e = new Exception();
+            throw e;
+        }
+        else {
+            this.recordList.get(index).addNote(note);
+        }
+    }
+
+    /**
+     * Generates progress summary
+     * @param userId the student to generate the record for.
+     * @returns the student's progress summary in a data class matching the JSON format.
+     * @throws Exception  if the progress summary could not be generated.
+     * SEE NOTE IN CLASS HEADER.
+     */
+    public ProgressSummary generateProgressSummary(String userId) throws Exception {
+        ProgressSummary nullProgressSummaryReturn = new ProgressSummary();
+        boolean realUser = false;
+        User desiredUser = new User();
+        for(User u : userList) {
+            if (userId.equals(u.getId())) {
+                desiredUser = u;
+                realUser = true;
+            }
+        }
+        //check if correct permissions for accessing student progress summary
+        if(realUser &&
+                ((currentUser.getRole().equals(Role.GRADUATE_PROGRAM_COORDINATOR) &&
+                        currentUser.getDepartment().equals(desiredUser.getDepartment())) ||
+                        currentUser.getId().equals(userId))) {
+            //finds studentRecord from data passed in with loadRecords and gets progressSummary
+            for(StudentRecord sr : recordList) {
+                if (userId.equals(sr.getStudent().getId())) {
+                    ProgressSummary progressSummary = new ProgressSummary(sr.getStudent(),
+                            sr.getDepartment(), sr.getDegreeSought(), sr.getTermBegan(),
+                            sr.getAdvisors(), sr.getCommittee(), sr.getNotes());
+                    progressSummary.checkGradStatus();
+                    return progressSummary;
+                }
+            }
+        }
+        else{
+            //throw exception
+        }
+        return nullProgressSummaryReturn;
+    }
+
+    /**
+     * Generates a new progress summary, assuming that the student passes the
+     * provided set of prospective courses.
+     * @param userId the student to generate the record for.
+     * @param courses a list of the prospective courses.
+     * @returns a map containing the student's hypothetical progress summary
+     * @throws Exception  if the progress summary could not be generated or the courses
+     * are invalid. SEE NOTE IN CLASS HEADER.
+     */
+    public ProgressSummary simulateCourses(String userId, List<CourseTaken> courses) throws Exception {
+        ProgressSummary progressSummary = new ProgressSummary();
+        return progressSummary;
+    }
 }
 
--- src/edu/umn/csci5801/model/StudentRecord.java
+++ src/edu/umn/csci5801/model/StudentRecord.java
@@ -1,83 +1,68 @@
 package edu.umn.csci5801.model;
 
-import java.util.List;
-
 /**
  * Created by Justin on 12/2/2014.
  */
-public class StudentRecord {
 
+import java.util.List;
+
+public class StudentRecord {
     private Student student;
     private Department department;
-    private Role role;
     private Degree degreeSought;
     private Term termBegan;
-    private List<Professor> advisor;
+    private List<Professor> advisors;
     private List<Professor> committee;
     private List<CourseTaken> coursesTaken;
     private List<CompletedMilestone> milestonesSet;
     private List<String> notes;
 
+    public StudentRecord() {}
     public StudentRecord(Student s, Department dep, Role r, Degree deg, Term tb, List<Professor> a, List<Professor> c,
                          List<CourseTaken> ct, List<String> n){
         student = s;
         department = dep;
         degreeSought = deg;
         termBegan = tb;
-        advisor = a;
+        advisors = a;
         committee = c;
         coursesTaken = ct;
         notes = n;
     }
 
-    public Degree getDegreeSought() {
-        return degreeSought;
-    }
+    public void setStudent(Student s) { student = s; }
+    public void setDepartment(Department d) { department = d; }
+    public void setDegreeSought(Degree d) { degreeSought = d; }
+    public void setTermBegan(Term t) { termBegan = t; }
+    public void setAdvisors(List<Professor> list) { advisors = list; }
+    public void addAdvisor(Professor p) { advisors.add(p); }
+    public void setCommittee(List<Professor> list) { committee = list; }
+    public void addCommitteeMember(Professor p) { committee.add(p); }
+    public void setCoursesTaken(List<CourseTaken> list) { coursesTaken = list; }
+    public void addCourseTaken(CourseTaken courseTaken) { coursesTaken.add(courseTaken); }
+    public void setMilestonesSet(List<CompletedMilestone> list) { milestonesSet = list; }
+    public void addMilestonesSet(CompletedMilestone completedMilestone) { milestonesSet.add(completedMilestone); }
+    public void setNotes(List<String> list) { notes = list; }
+    public void addNote(String s) { notes.add(s); }
 
-    public Department getDepartment() {
-        return department;
-    }
+    public Student getStudent() { return student; }
+    public Department getDepartment() { return department; }
+    public Degree getDegreeSought() { return degreeSought; }
+    public Term getTermBegan() { return termBegan; }
+    public List<Professor> getAdvisors() { return advisors; }
+    public List<Professor> getCommittee() { return committee; }
+    public List<CourseTaken> getCoursesTaken() { return coursesTaken; }
+    public List<CompletedMilestone> getMilestonesSet() { return milestonesSet; }
+    public List<String> getNotes() { return notes; }
 
-    public List<Professor> getAdvisor() {
-        return advisor;
-    }
-
-    public List<Professor> getCommittee() {
-        return committee;
-    }
-
-    public List<String> getNotes() {
-        return notes;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public Term getTermBegan() {
-        return termBegan;
-    }
-
-    public List<CourseTaken> getCoursesTaken() {
-        return coursesTaken;
-    }
-//
-    public List<CompletedMilestone> getMilestonesSet() {
-        return milestonesSet;
-    }
-
-//    public ProgressSummary checkProgressSummary(Degree degree, List<CourseTaken> coursesTaken){
-//
-//        return progress;
-//    }
-//
     public void appendCourses(List<CourseTaken> coursesAppended){
-        this.coursesTaken = coursesAppended;
-
+        for(CourseTaken courseTaken : coursesAppended) {
+            this.coursesTaken.add(courseTaken);
+        }
     }
 
+    public ProgressSummary checkProgressSummary(Degree d, List<CourseTaken> coursesTaken) {
+        ProgressSummary progressSummary = new ProgressSummary();
+        return progressSummary;
+    }
 }
