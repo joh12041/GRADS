25e44fcce87c1e217b4da0abc66c5ec8e8b30b62
Isaac J <joh12041@umn.edu>
2014-12-03 00:03:58 -0600

    Update ProgressSummary.java

--- src/edu/umn/csci5801/model/ProgressSummary.java
+++ src/edu/umn/csci5801/model/ProgressSummary.java
@@ -1,8 +1,22 @@
 package edu.umn.csci5801.model;
 
+import java.util.List;
+
 /**
  * Created by Justin on 12/2/2014.
  */
 public class ProgressSummary {
-    public void checkGradStatus() {}
+    private Student student;
+    private Department department;
+    private Degree degree;
+    private Term termBegan;
+    private List<Professor> advisors;
+    private List<Professor> committee;
+    private List<GradReqCheck> reqCheckResult;
+    private List<String> notes;
+
+
+    public void checkGradStatus() {
+
+    }
 }
