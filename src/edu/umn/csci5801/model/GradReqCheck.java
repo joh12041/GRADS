b981abc22576afd8577b41739a0485097cecb14b
Justin Wells <wells369@umn.edu>
2014-12-03 13:09:27 -0600

    GradReqCheck.java

--- src/edu/umn/csci5801/model/GradReqCheck.java
+++ src/edu/umn/csci5801/model/GradReqCheck.java
@@ -1,10 +1,32 @@
 package edu.umn.csci5801.model;
 
-import java.util.List;
-
 /**
  * Created by Justin on 12/2/2014.
  */
 public class GradReqCheck {
+    private String reqName;
+    private boolean result;
+    private Requirement details;
 
+    public GradReqCheck() {}
+    public GradReqCheck(String n, boolean ic, Requirement r) {
+        reqName = n;
+        result = ic;
+        details = r;
+    }
+
+    public void setReqName(String n) { reqName = n; }
+    public void setResult(boolean ic) { result = ic; }
+    public void setDetails(Requirement r) { details = r; }
+
+    public String getReqName() { return reqName; }
+    public boolean getResult() { return result; }
+    public Requirement getDetails() { return details; }
+
+    public Requirement buildReq(Requirement requirement) {
+        return requirement;
+    }
+    public void testReq(Requirement requirement) {
+        return;
+    }
 }
