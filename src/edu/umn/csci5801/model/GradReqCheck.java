1101c968c668e1d288d4dc56a9fa1107f06ea038
joh12041 <joh12041@umn.edu>
2014-12-03 23:19:57 -0600

    Update Requirements.name to Reqs enum

--- src/edu/umn/csci5801/model/GradReqCheck.java
+++ src/edu/umn/csci5801/model/GradReqCheck.java
@@ -6,20 +6,20 @@ import java.util.List;
  * Created by Justin on 12/2/2014.
  */
 public class GradReqCheck {
-    private String reqName;
+    private Reqs reqName;
     private boolean result;
     private Requirement details;
 
     public GradReqCheck() {}
-    public GradReqCheck(String n) {
+    public GradReqCheck(Reqs n) {
         reqName = n;
     }
 
-    public void setReqName(String n) { reqName = n; }
+    public void setReqName(Reqs n) { reqName = n; }
     public void setResult(boolean ic) { result = ic; }
     public void setDetails(Requirement r) { details = r; }
 
-    public String getReqName() { return reqName; }
+    public Reqs getReqName() { return reqName; }
     public boolean getResult() { return result; }
     public Requirement getDetails() { return details; }
 
@@ -28,7 +28,7 @@ public class GradReqCheck {
         //TODO: Logic for checking any given requirement
         switch (requirement.getName()) {
             //TODO: Fill in all the different case statements (one for each possible requirement name
-            case "BREADTH_REQUIREMENT_MS":
+            case BREADTH_REQUIREMENT_MS:
         }
     }
 }
