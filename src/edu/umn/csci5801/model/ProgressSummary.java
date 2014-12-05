557669e31428b638a8848f4a7ad437b8f7d916cd
Christian Drews <drews032@umn.edu>
2014-12-04 19:51:31 -0600

    JavaDocs (notFullyCorrect) added to files

--- src/edu/umn/csci5801/model/ProgressSummary.java
+++ src/edu/umn/csci5801/model/ProgressSummary.java
@@ -1,9 +1,28 @@
+/**
+ * Copyright 2014 CSCI5801 Fall2014 Group1
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.umn.csci5801.model;
 
 import java.util.List;
 
 /**
- * Created by Justin on 12/2/2014.
+ * ProgressSummary.java - Class that handles the status of a Requirement
+ * 
+ * @author CSCI5801 Fall2014 Group1
+ * @version 1.0
  */
 public class ProgressSummary {
 
@@ -17,9 +36,10 @@ public class ProgressSummary {
     private List<String> notes;
     private List<CourseTaken> coursesTaken;
 
-    //Default Constructor
+    /**
+     * ProgressSummary() - Constructors for creating a ProgressSummary object
+     */
     public ProgressSummary() {}
-    //Constructor
     public ProgressSummary(Student s, Department dep, Degree deg, Term tb,
                            List<Professor> a, List<Professor> c, List<String> n, List<CourseTaken> ct) {
         student = s;
@@ -32,12 +52,20 @@ public class ProgressSummary {
         coursesTaken = ct;
     }
 
+    /**
+     * checkGradStatus() - Method for checking how close a student is to graduating
+     * 
+     * @param completedMilestonesList - Input of type List<CompletedMilestone> which contains a student's completed milestones
+     */
     public void checkGradStatus(List<CompletedMilestone> completedMilestonesList) throws Exception {
         GradRequirements gradRequirements = new GradRequirements(degree);
         gradRequirements.testReqList(coursesTaken, completedMilestonesList);
         reqCheckResult = gradRequirements.getReqChecks();
     }
 
+    /**
+     * Methods for getting attributes of a ProgressSummary object
+     */
     public List<GradReqCheck> getReqCheckResult() {
         return reqCheckResult;
     }
--- src/edu/umn/csci5801/model/Requirement.java
+++ src/edu/umn/csci5801/model/Requirement.java
@@ -1,10 +1,29 @@
+/**
+ * Copyright 2014 CSCI5801 Fall2014 Group1
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.umn.csci5801.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Justin on 12/2/2014.
+ * Requirement.java - Class that handles the status of a Requirement
+ * 
+ * @author CSCI5801 Fall2014 Group1
+ * @version 1.0
  */
 public class Requirement {
     private Reqs name;
@@ -14,7 +33,9 @@ public class Requirement {
     private List<CompletedMilestone> milestones;
     private List<String> notes;
 
-    //constructor
+    /**
+     * Requirement() - Constructors for creating a Requirement object
+     */
     public Requirement(){}
     public Requirement(Reqs _name_) {
         name = _name_;
@@ -57,23 +78,65 @@ public class Requirement {
         notes = _notes_;
     }
 
-    public void setName(Reqs n) { name = n; }
-    public void setGpa(double g_p_a) { gpa = g_p_a; }
-    public void setCourses(List<CourseTaken> coursesTaken) { courses = coursesTaken; }
-    public void addCourse(CourseTaken ct) { courses.add(ct); }
-    public void setCredits(int c) { credits = c; }
-    public void setMilestones(List<CompletedMilestone> list) { milestones = list; }
-    public void addMilestone(CompletedMilestone cm) { milestones.add(cm); }
-    public void setNotes(List<String> list) { notes = list; }
-    public void addNote(String note) { notes.add(note); }
+    /**
+     * Methods for setting or adding attributes of a Requirement object
+     */
+    public void setName(Reqs n) {
+    	name = n;
+    }
+    public void setGpa(double g_p_a) {
+    	gpa = g_p_a;
+    }
+    public void setCourses(List<CourseTaken> coursesTaken) {
+    	courses = coursesTaken;
+    }
+    public void addCourse(CourseTaken ct) {
+    	courses.add(ct);
+    }
+    public void setCredits(int c) {
+    	credits = c;
+    }
+    public void setMilestones(List<CompletedMilestone> list) {
+    	milestones = list;
+    }
+    public void addMilestone(CompletedMilestone cm) {
+    	milestones.add(cm);
+    }
+    public void setNotes(List<String> list) {
+    	notes = list;
+    }
+    public void addNote(String note) {
+    	notes.add(note);
+    }
 
-    public double getGpa() { return gpa; }
-    public int getCredits() { return credits; }
-    public List<CompletedMilestone> getMilestones() { return milestones; }
-    public List<CourseTaken> getCourses() { return courses; }
-    public List<String> getNotes() { return notes; }
-    public Reqs getName() { return name; }
+    /**
+     * Methods for getting attributes of a Requirement object
+     */
+    public double getGpa() {
+    	return gpa;
+    }
+    public int getCredits() {
+    	return credits;
+    }
+    public List<CompletedMilestone> getMilestones() {
+    	return milestones;
+    }
+    public List<CourseTaken> getCourses() {
+    	return courses;
+    }
+    public List<String> getNotes() {
+    	return notes;
+    }
+    public Reqs getName() {
+    	return name;
+    }
 
+    /**
+     * gradeToDouble() - Method for mapping letter grades to numerical grades
+     * 
+     * @param grade - Input of type Grade which contains a letter grade
+     * @return gradeD - Returns a Double containing a numerical value for a grade
+     */
     private double gradeToDouble(Grade grade){
         double gradeD = 0.0;
         switch (grade) {
@@ -93,6 +156,9 @@ public class Requirement {
         return gradeD;
     }
 
+    /**
+     * calculateGpa() - Method for determining the GPA of a set of courses
+     */
     public void calculateGpa() {
         double totalGradePoints = 0;
         int totalCourseCredits = 0;
